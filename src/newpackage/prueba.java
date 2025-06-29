036ae1caaa63b039d7c9f48172782ed997fb18af
ItsFern27 <fjarevalo07@gmail.com>
2025-06-29 17:27:56 -0500

    Actuaklizacioon

--- src/newpackage/prueba.java
+++
@@ -1,8 +0,0 @@
-package newpackage;
-
-public class prueba {
-    public static void main(String[] args) {
-        Cliente pija = new Cliente(71395898);
-        System.out.println(pija.getId());
-    }
-}
