3a4270b474d02026b11711a2f5e9e9eb22860ecb
RenzoPM62 <renzo.prada6212@gmail.com>
2025-06-29 19:19:50 -0500

    Clase Main agregada

---
+++ src/newpackage/Main.java
@@ -0,0 +1,25 @@
+package newpackage;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        GestionCola gestionCola = new GestionCola();
+
+        // Mostrar primero la ventana de ingreso de cliente
+        SwingUtilities.invokeLater(() -> {
+            VentanaIngresoCliente ventanaIngreso = new VentanaIngresoCliente(gestionCola);
+            ventanaIngreso.setVisible(true);
+
+            // Cuando se cierre la ventana de ingreso, mostrar la ventana de la cola
+            ventanaIngreso.addWindowListener(new java.awt.event.WindowAdapter() {
+                @Override
+                public void windowClosing(java.awt.event.WindowEvent e) {
+                    ventanaIngreso.dispose();
+                    VentanaMostrarCola ventanaCola = new VentanaMostrarCola(gestionCola);
+                    ventanaCola.setVisible(true);
+                }
+            });
+        });
+    }
+}
